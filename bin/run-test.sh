ad1c628177ddcbc95c4b2497137a713ceabea8e3
Ali Ijaz Sheikh <ofrobots@google.com>
2017-05-28 08:47:20 -0700

    more debugging

--- bin/run-test.sh
+++ bin/run-test.sh
@@ -2,54 +2,54 @@
 
 # Usage: -c to report coverage
 
-while true; do
-  case $1 in
-    -c)
-      cover=1
-      ;;
+# while true; do
+#   case $1 in
+#     -c)
+#       cover=1
+#       ;;
 
-    *)
-      break
-  esac
+#     *)
+#       break
+#   esac
 
-  shift
-done
+#   shift
+# done
 
-# Lint
-jshint . || exit 1
+# # Lint
+# jshint . || exit 1
 
-./bin/install-test-fixtures.sh
+# ./bin/install-test-fixtures.sh
 
-# Get test/coverage command
-counter=0
-function run {
-  C="$(npm bin)/istanbul test"
-  if [ "$cover" ]; then
-    C="$(npm bin)/istanbul cover --dir ./coverage/${counter}"
-    ((counter++))
-  fi
-  ($C "$(npm bin)/_mocha" -- $* --timeout 4000 --R spec) || exit 1
-}
+# # Get test/coverage command
+# counter=0
+# function run {
+#   C="$(npm bin)/istanbul test"
+#   if [ "$cover" ]; then
+#     C="$(npm bin)/istanbul cover --dir ./coverage/${counter}"
+#     ((counter++))
+#   fi
+#   ($C "$(npm bin)/_mocha" -- $* --timeout 4000 --R spec) || exit 1
+# }
 
-# Run test/coverage
-for test in test/test-*.js test/plugins/*.js ;
-do
-# not v0.12 or not koa = not (v0.12 and koa)
-  if [[ ! $(node --version) =~ v0\.12\..* || ! "${test}" =~ .*trace\-(koa|google\-gax)\.js ]]
-  then
-    run "${test}"
-  fi
-done
+# # Run test/coverage
+# for test in test/test-*.js test/plugins/*.js ;
+# do
+# # not v0.12 or not koa = not (v0.12 and koa)
+#   if [[ ! $(node --version) =~ v0\.12\..* || ! "${test}" =~ .*trace\-(koa|google\-gax)\.js ]]
+#   then
+#     run "${test}"
+#   fi
+# done
 
-# Conditionally publish coverage
-if [ "$cover" ]; then
-  istanbul report lcovonly
-  ./node_modules/coveralls/bin/coveralls.js < ./coverage/lcov.info
-  rm -rf ./coverage
-fi
+# # Conditionally publish coverage
+# if [ "$cover" ]; then
+#   istanbul report lcovonly
+#   ./node_modules/coveralls/bin/coveralls.js < ./coverage/lcov.info
+#   rm -rf ./coverage
+# fi
 
-# Run non-interference tests
-node test/non-interference/http-e2e.js || exit 1
+# # Run non-interference tests
+# node test/non-interference/http-e2e.js || exit 1
 
 # When running locally, or on non-PR builds on travis, run the system tests.
 if [[ (-z "${CIRCLECI}" && -z "${APPVEYOR}" && -z "${TRAVIS_PULL_REQUEST}") || \
